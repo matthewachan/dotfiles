78c0096255aa0002cd69af11eb3fa879b3b38377
Matthew Chan <matthew.a.chan@gmail.com>
2018-03-07 20:04:35 -0800

    Update to use symlinks

--- setup.sh
+++ setup.sh
@@ -1,19 +1,29 @@
-#!/bin/bash
+# !/bin/bash
 
-# Clone vundle
+# @author Matthew chan
+# @date March 7, 2018
+# @description  Set up script to install vim with all of my preferred plugins and configs
+# @dependencies Vim 8.0, Python (either 2.7 or 3), Jedi (a Python autocomplete package):w
+
+# Clone Vundle, our vim package manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Vim config 
-mv .vimrc ~/.vimrc
-vim +PluginInstall +qall # Install all Vundle plugins
+# Create symbolic link in the home dir to our .vimrc file
+ln -s .vimrc ~/.vimrc
 
-# Add bash config 
-echo "TERM=xterm-256color" >> .bashrc
+# Install Vundle plugins
+vim +PluginInstall +qall 
 
-# Add screen config 
-echo "altscreen on" >> .screenrc
-echo "term screen-256color" >> .screenrc
-echo "shell -$SHELL" >> .screenrc
+##########################
+# Optional configuration #
+##########################
 
-# Clean up
-rm -rf ../dotfiles
+# Enable 256 color terminal
+# echo "TERM=xterm-256color" >> .bashrc
+
+# Enable alternate screen (removes terminal output from vi after closing vi)
+# echo "altscreen on" >> .screenrc
+# Enable 256 color terminal
+# echo "term screen-256color" >> .screenrc
+# Loads bash configs into screen (i.e. aliases and path)
+# echo "shell -$SHELL" >> .screenrc
