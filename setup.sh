7f7cfd1133e48906118a373a68b27ea7bc4406e7
Matthew Chan <matthew.a.chan@gmail.com>
2018-03-18 09:22:29 -0700

    Ensure stability

--- setup.sh
+++ setup.sh
@@ -3,17 +3,32 @@
 # @author Matthew chan
 # @date March 7, 2018
 # @description  Set up script to install vim with all of my preferred plugins and configs
-# @dependencies Vim 8.0, Python (either 2.7 or 3), Jedi (a Python autocomplete package):w
+# @dependencies Vim 8.0, Python (either 2.7 or 3), Jedi (a Python autocomplete package)
 
 # Clone Vundle, our vim package manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Create symbolic link in the home dir to our .vimrc file
+# Create symbolic link in the home dir to our configuration files
 # NOTE: Moving the .vimrc file in the repo will break the symlink
 ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.screenrc ~/.screenrc
+
 
 # Install Vundle plugins
 vim +PluginInstall +qall 
+
+# Rename .vimrc file if it exists
+if [ -f "~/.vimrc" ]; then
+  mv ~/.vimrc ~/.vimrc_$(date +%Y%s%m%3N)
+fi
+
+# Create colorscheme dir (if it doesn't already exist)
+if [ ! -d "~/.vim/colors" ]; then
+  mkdir ~/.vim/colors
+fi
+
+# Move colorscheme files into colorscheme dir
+mv ~/.vim/bundle/vim-colorschemes/colors/* ~/.vim/colors
 
 ##########################
 # Optional configuration #
@@ -21,10 +36,3 @@ vim +PluginInstall +qall
 
 # Enable 256 color terminal
 # echo "TERM=xterm-256color" >> .bashrc
-
-# Enable alternate screen (removes terminal output from vi after closing vi)
-# echo "altscreen on" >> .screenrc
-# Enable 256 color terminal
-# echo "term screen-256color" >> .screenrc
-# Loads bash configs into screen (i.e. aliases and path)
-# echo "shell -$SHELL" >> .screenrc
